f3c7c8e116e9360e54d690d0aace1dc3309b3782
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-19 01:11:08 -0600

    added changes

--- server-side/cookies-spring-webmvc/cookies-spring-services/src/main/java/edu/utdallas/wpl/cookies/spring/services/impl/AddressRestServiceImpl.java
+++ server-side/cookies-spring-webmvc/cookies-spring-services/src/main/java/edu/utdallas/wpl/cookies/spring/services/impl/AddressRestServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import edu.utdallas.wpl.cookies.spring.biz.manager.AddressServiceManager;
 import edu.utdallas.wpl.cookies.spring.common.dto.Address;
 import edu.utdallas.wpl.cookies.spring.services.AddressRestService;
-@CrossOrigin
+
 @Controller 
 @RequestMapping("/api")
 public  class AddressRestServiceImpl implements AddressRestService {
