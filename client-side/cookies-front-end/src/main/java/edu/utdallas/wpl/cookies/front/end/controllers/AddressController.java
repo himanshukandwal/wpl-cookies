1056cfdc9a5f492f823f8c7d7390fc7e88af412b
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-19 19:34:56 -0600

    added address service controller

--- client-side/cookies-front-end/src/main/java/edu/utdallas/wpl/cookies/front/end/controllers/AddressController.java
+++ client-side/cookies-front-end/src/main/java/edu/utdallas/wpl/cookies/front/end/controllers/AddressController.java
@@ -5,9 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import edu.utdallas.wpl.cookies.spring.common.dto.Address;
 
 @RestController
 public class AddressController {
@@ -18,10 +23,25 @@ public class AddressController {
 	@Value("${services.url}")
 	private String webserviceUrl;
 	
+	@RequestMapping(value = "/address", method = RequestMethod.POST)
+	public Map<String, Object> createAddress(@RequestBody Map<String, Object> addressMap) {
+		Map<String,Object> model = new HashMap<String,Object>();
+		
+		// web service invocation.
+		ResponseEntity<Address> responseEntity = restTemplate.postForEntity(webserviceUrl + "/addresses", addressMap, Address.class);
+				
+		model.put("address", responseEntity.getBody());
+		return model;
+	}
+	
 	@RequestMapping("/addresses")
 	public Map<String, Object> getAddresses() {
 		Map<String,Object> model = new HashMap<String,Object>();
-		model.put("status", "success");
+		
+		// web service invocation.
+		ResponseEntity<Address[]> responseEntity = restTemplate.getForEntity(webserviceUrl + "/addresses", Address[].class);
+				
+		model.put("addresses", responseEntity.getBody());
 		return model;
 	}
 
