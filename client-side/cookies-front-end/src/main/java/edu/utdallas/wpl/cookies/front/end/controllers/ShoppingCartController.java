e17f030eeeff8974a37049537788434a4e90277d
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-11-29 17:25:41 -0600

    added configuration things

---
+++ client-side/cookies-front-end/src/main/java/edu/utdallas/wpl/cookies/front/end/controllers/ShoppingCartController.java
@@ -0,0 +1,63 @@
+package edu.utdallas.wpl.cookies.front.end.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import edu.utdallas.wpl.cookies.front.end.config.CustomRestController;
+import edu.utdallas.wpl.cookies.spring.common.dto.TransactionInfo;
+
+@CustomRestController
+public class ShoppingCartController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${services.url}")
+	private String webserviceUrl;
+	
+//	@RequestMapping(value = "/getCartItems/{userId}", method = RequestMethod.POST)
+//	public @ResponseBody Map<String, Object> createTransaction(@RequestBody Map<String, Object> transactionMap) {
+//		Map<String,Object> model = new HashMap<String,Object>();
+//		
+//		// web service invocation.
+//		ResponseEntity<TransactionInfo> responseEntity = restTemplate.postForEntity(webserviceUrl + "/saveInterestedBid", transactionMap, TransactionInfo.class);
+//				
+//		model.put("transaction", responseEntity.getBody());
+//		return model;
+//	}
+//	
+//	@RequestMapping(value = "/updateTransaction/{bidStatus}", method = RequestMethod.PUT)
+//	public @ResponseBody Map<String, Object> updateTransaction(@PathVariable String bidStatus, @RequestBody Map<String, Object> transactionMap) {
+//		Map<String,Object> model = new HashMap<String,Object>();
+//		
+//		// web service invocation.
+//		restTemplate.put(webserviceUrl + "/updateBidStatus/" + bidStatus, transactionMap);
+//				
+//		TransactionInfo result = restTemplate.getForObject(webserviceUrl + "/getTransactionById/" + transactionMap.get("tranId"), TransactionInfo.class);
+//
+//		model.put("transaction", result);
+//		return model;		
+//	}
+//	
+//	@RequestMapping(value = "/getTransactions/{bidId}", method = RequestMethod.GET)
+//	public @ResponseBody Map<String, Object> getTransactionsByBId(@PathVariable Integer bidId) {
+//		Map<String,Object> model = new HashMap<String,Object>();
+//		
+//		// web service invocation.
+//		ResponseEntity<TransactionInfo[]> responseEntity = restTemplate.getForEntity(webserviceUrl + "/getTransactionByBid/" + bidId, TransactionInfo[].class);
+//				
+//		model.put("transaction", responseEntity.getBody());
+//		return model;
+//	}
+	
+}
--- client-side/cookies-front-end/src/main/resources/static/js/modules/bidding.js
+++ client-side/cookies-front-end/src/main/resources/static/js/modules/bidding.js
@@ -30,6 +30,12 @@ angular.module('biddingModule', ['ui.router', 'angular.filter', 'ngAnimate', 'sm
                 templateUrl : '../../templates/user-profile/bid-detail.html',
                 params : { userInfo : null, bid : null },
                 controller : 'bid-detail'
+            })
+            .state('user-shopping-cart', {
+                url : '/user-profile/user-shopping-cart',
+                templateUrl : '../../templates/user-profile/shopping-cart.html',
+                params : { userInfo : null },
+                controller : 'shopping-cart'
             });
     })
     .controller('find-address', function ($http, $timeout, $stateParams, $state, $scope) {
@@ -159,12 +165,62 @@ angular.module('biddingModule', ['ui.router', 'angular.filter', 'ngAnimate', 'sm
             $http.put('/api/updateTransaction/FINALISED', transaction).then(function () {
                 transaction.bidStatus = 'FINALISED';
                 console.log('updated status successfully.');
+
             }, function (response) {
                 console.log(response.data.status);
                 self.message = "error updating user !";
             });
         };
         
+        self.createTransactionItem = function () {
+            self.message = false;
+
+            $http.post('/api/createTransaction', self.transaction).then(function (response) {
+                console.log(response.data.status);
+                self.message = false;
+                self.bidTransactions.push(response.data.transaction);
+
+                delete self.transaction.comments;
+                delete self.transaction.bidPrice;
+            }, function (response) {
+                console.log(response.data.status);
+                self.message = "error publishing interest !";
+
+                delete self.transaction.comments;
+                delete self.transaction.bidPrice;
+            });
+        };
+
+    })
+    .controller('shopping-cart', function($http, $stateParams) {
+        var self = this;
+        self.userInfo = $stateParams.userInfo;
+
+        self.transaction = { bid : self.bid, bidReceiver : self.userInfo, bidStatus : 'INTERESTED' };
+
+        self.bidTransactions = [];
+
+        self.isMyBid = self.bid.owner.id == self.userInfo.id;
+
+        $http.get('/api/getTransactions/' + self.bid.bidId).then(function (response) {
+            self.bidTransactions = response.data.transaction;
+        }, function (response) {
+            console.log(response.data);
+        });
+
+        self.selectTransaction = function (transaction) {
+            delete transaction.$$hashKey;
+
+            $http.put('/api/updateTransaction/FINALISED', transaction).then(function () {
+                transaction.bidStatus = 'FINALISED';
+                console.log('updated status successfully.');
+
+            }, function (response) {
+                console.log(response.data.status);
+                self.message = "error updating user !";
+            });
+        };
+
         self.createTransactionItem = function () {
             self.message = false;
 
