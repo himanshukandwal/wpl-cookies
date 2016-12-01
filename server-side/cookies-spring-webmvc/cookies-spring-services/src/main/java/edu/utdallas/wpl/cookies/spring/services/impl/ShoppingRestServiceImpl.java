f3a545527eea11c13d765d50cc085902671a42db
anirudh-valsalan <anirudh123kv@gmail.com>
2016-11-30 18:29:59 -0600

    email added

--- server-side/cookies-spring-webmvc/cookies-spring-services/src/main/java/edu/utdallas/wpl/cookies/spring/services/impl/ShoppingRestServiceImpl.java
+++ server-side/cookies-spring-webmvc/cookies-spring-services/src/main/java/edu/utdallas/wpl/cookies/spring/services/impl/ShoppingRestServiceImpl.java
@@ -2,7 +2,15 @@ package edu.utdallas.wpl.cookies.spring.services.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -66,9 +74,9 @@ public class ShoppingRestServiceImpl implements ShoppingRestService {
 		for (TransactionInfo transactionInfo : transactionInfoList) {
 
 			ShoppingInfo shoppingInfoReq = convertTransactionToshopping(transactionInfo);
-			ShoppingServiceManager.addShoppingInfo(shoppingInfoReq);
-			shoppingInfoList.add(shoppingInfoReq);
-			LOG.info(" created user with id :" + shoppingInfoReq.getShoppingId());
+			ShoppingInfo shoppingInfoRes=ShoppingServiceManager.addShoppingInfo(shoppingInfoReq);
+			shoppingInfoList.add(shoppingInfoRes);
+			LOG.info(" created user with id :" + shoppingInfoRes.getShoppingId());
 		}
 
 		for (TransactionInfo transactionInfo : transactionInfoList) {
@@ -76,6 +84,9 @@ public class ShoppingRestServiceImpl implements ShoppingRestService {
 			String bidPoster = transactionInfo.getBid().getOwner().getEmail();
 			String bidReceiver = transactionInfo.getBidReceiver().getEmail();
 			Integer bidId = transactionInfo.getBid().getBidId();
+			System.out.println("Bid poster>"+bidPoster);
+			System.out.println("Bid receiver>"+bidReceiver+" bid id "+bidId);
+		//	sendMail(from, password, bidPoster, bidReciver);
 
 		}
 
@@ -92,4 +103,77 @@ public class ShoppingRestServiceImpl implements ShoppingRestService {
 		return shoppingInfo;
 	}
 
+	
+	private boolean sendMail(String from, String password, String bidPoster,String bidReciver) {
+		String host = "smtp.gmail.com";
+		Properties properties = System.getProperties();
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.user", host);
+		properties.put("mail.smtp.password", password);
+		properties.put("mail.smtp.port", 587);
+		properties.put("mail.smtp.auth", "true");
+		Session session = Session.getDefaultInstance(properties, null);
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+			mimeMessage.setFrom(new InternetAddress(from));
+
+			InternetAddress toAddress = new InternetAddress();
+			toAddress.setAddress(bidPoster);
+            mimeMessage.addRecipient(RecipientType.TO, toAddress);
+			mimeMessage.setSubject("Your BID Receipt from Cookies");
+			String messageIntro = "<div style=\"color:black;\"><br>Hi </br></div>";
+			String messgeToBidPoster= "<div><body> <br>Thank you very much for purchasing the items through cookie bidding service</br></body></div>";
+			String messageSalutaion="<br></br><div><body> Thanks And Regards,<br> cookie admin</br></body></div>";
+			mimeMessage.setContent(messageIntro+messgeToBidPoster+messageSalutaion, "text/html");
+ 
+			Session session2 = Session.getDefaultInstance(properties, null);
+			MimeMessage mimeMessage2 = new MimeMessage(session2);
+			InternetAddress toAddress2 = new InternetAddress();
+			toAddress2.setAddress(bidReciver);
+			mimeMessage2.addRecipient(RecipientType.TO, toAddress2);
+			mimeMessage2.setSubject("Congrats!");
+			
+			
+			sendMessageToBidAgents(session2, host, from,password,bidPoster,mimeMessage);
+			String messageToBidReciver ="<div><body> <br> Congrats your Item has beeen purchased.Thank you very much for posting Item through cookie bidding service</br></body></div>";
+			mimeMessage2.setContent(messageIntro+messageToBidReciver+messageSalutaion, "text/html");
+			sendMessageToBidAgents(session2,host,from, password,bidReciver, mimeMessage2);
+			
+			
+			
+			return true;
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+	
+	
+	
+	
+	
+	
+	private static void sendMessageToBidAgents(Session session, String host, String from,String password, String bidPoster,
+			MimeMessage mimeMessage) {
+		
+		Transport transport;
+		try {
+			transport = session.getTransport("smtp");
+		
+		transport.connect(host, from, password);
+		transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+		transport.close();
+		} catch ( MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
