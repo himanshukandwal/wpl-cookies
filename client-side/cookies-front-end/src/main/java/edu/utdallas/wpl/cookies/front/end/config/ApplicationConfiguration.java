4b6cd8502dd87c71279825f33bf193e4db818c7e
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-12-03 14:55:43 -0600

    completed SSL changes

--- client-side/cookies-front-end/src/main/java/edu/utdallas/wpl/cookies/front/end/config/ApplicationConfiguration.java
+++ client-side/cookies-front-end/src/main/java/edu/utdallas/wpl/cookies/front/end/config/ApplicationConfiguration.java
@@ -11,80 +11,77 @@ import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomize
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfiguration {
 
-//	@Value("")
-//	private Resource keystoreFile;
-//
-//	@Value("${server.ssl.key-store-password}")
-//	private String keystorePass;
-//
-//	@Value("${server.ssl.keyStoreType}")
-//	private String keystoreType;
-//
-//	@Value("${server.ssl.keyAlias}")
-//	private String keystoreAlias;
-//
-//	@Value("${server.port}")
-//	private int tlsPort;
+	@Value("${server.ssl.key-store}")
+	private String keystoreFile;
+
+	@Value("${server.ssl.key-store-password}")
+	private String keystorePass;
+
+	@Value("${server.ssl.keyStoreType}")
+	private String keystoreType;
+
+	@Value("${server.ssl.keyAlias}")
+	private String keystoreAlias;
+
+	@Value("${server.port}")
+	private int tlsPort;
 
 	@Bean
 	public RestTemplate restTemplate() {
-//		// for localhost (self-signed certificate) handshaking.
-//		try {
-//			CloseableHttpClient httpClient = HttpClients.custom()
-//		        .setSSLHostnameVerifier(new NoopHostnameVerifier())
-//		        .build();
-//			
-//		    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//		    requestFactory.setHttpClient(httpClient);
-//
-//			System.out.println("creating Rest template woth request factory !");
-//			return new RestTemplate(requestFactory);
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		// for localhost (self-signed certificate) handshaking.
+		try {
+			CloseableHttpClient httpClient = HttpClients.custom()
+		        .setSSLHostnameVerifier(new NoopHostnameVerifier())
+		        .build();
+			
+		    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		    requestFactory.setHttpClient(httpClient);
+
+			System.out.println("creating Rest template with request factory !");
+			return new RestTemplate(requestFactory);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return new RestTemplate();
 	}
 
-//	@Bean
-//	public EmbeddedServletContainerFactory servletContainer() {
-//		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-//
-//		factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-//
-//			@Override
-//			public void customize(Connector connector) {
-//
-//				connector.setPort(tlsPort);
-//				connector.setSecure(true);
-//				connector.setScheme("https");
-//				connector.setAttribute("keyAlias", "tomcat");
-//				connector.setAttribute("keystorePass", keystorePass);
-//				connector.setAttribute("keystoreFile",
-//						"/Users/Heman/Documents/workstation/Developement_Studio/Java_Laboratory/wpl-cookies/client-side/cookies-front-end/keystore.p12");
-//				connector.setAttribute("clientAuth", "false");
-//				connector.setAttribute("sslProtocol", "TLS");
-//				connector.setAttribute("SSLEnabled", true);
-//
-//				Http11NioProtocol proto = (Http11NioProtocol) connector.getProtocolHandler();
-//				proto.setSSLEnabled(true);
-//				proto.setKeystoreFile(
-//						"/Users/Heman/Documents/workstation/Developement_Studio/Java_Laboratory/wpl-cookies/client-side/cookies-front-end/keystore.p12");
-//				proto.setKeystorePass(keystorePass);
-//				proto.setKeystoreType(keystoreType);
-//				proto.setKeyAlias(keystoreAlias);
-//			}
-//		});
-//
-//		return factory;
-//	}
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+
+		factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+
+			@Override
+			public void customize(Connector connector) {
+
+				connector.setPort(tlsPort);
+				connector.setSecure(true);
+				connector.setScheme("https");
+
+				Http11NioProtocol proto = (Http11NioProtocol) connector.getProtocolHandler();
+				proto.setSSLEnabled(true);
+				proto.setKeystoreFile(keystoreFile);
+				proto.setKeystorePass(keystorePass);
+				proto.setKeystoreType(keystoreType);
+				proto.setKeyAlias(keystoreAlias);
+				proto.setSslProtocol("TLS");
+				
+				proto.setTruststoreFile(keystoreFile);
+				proto.setTruststorePass(keystorePass);
+				proto.setClientAuth("false");
+				proto.setTruststoreType(keystoreType);
+			}
+		});
+
+		return factory;
+	}
 
 }
